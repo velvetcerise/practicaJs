435677d973f646360dffa31a2137046c4ec30360
Angelina <alicona@bexponencial.com>
2020-05-05 17:20:24 -0500

    Se agregan pruebas para recoger valores de formulario

--- Ejercicio Unidad 3/js/javascript.js
+++ Ejercicio Unidad 3/js/javascript.js
@@ -1,3 +1,25 @@
+var saludo = "Hola, mi nombre es";
+var nombre = "Angie";
+var edad = 29;
+
+var fecha = 1991;
+var edad = 2020 - fecha;
+
+var mensaje = saludo + " " + nombre + " y tengo " + edad + " años";
+
+// Imprimir en consola
+console.log(mensaje);
+console.log(nombre + ", tienes " + edad + " años");
+
+
+// Selector querySelector 
+// Método .value (imprimimos valor)
+var getName = document.querySelector('#nombre').value;
+
+var getDate = document.querySelector('#fecha').value;
+
+console.log(getName);
+console.log(getDate);
 
 
 
