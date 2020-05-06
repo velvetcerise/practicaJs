4a69e13bb5c4b387deb05314f86d6058be74d3ed
Angelina <alicona@bexponencial.com>
2020-05-05 19:35:16 -0500

    Agrega contenido al DOM, al momento de enviar los datos del formulario

--- Ejercicio Unidad 3/js/javascript.js
+++ Ejercicio Unidad 3/js/javascript.js
@@ -7,27 +7,15 @@ function recogeDatos(e) {
   var nombre = document.querySelector('#nombre').value;
   var fecha = document.querySelector('#fecha').value;
   var edad = 2020 - fecha;
-  
-  console.log('Hola, ' + nombre + ", tienes " + edad + " años");
+  var bienvenida = document.querySelector('#bienvenida');
+  var mensaje = 'Hola, ' + nombre + ', tienes ' + edad + ' años';
+  bienvenida.textContent = mensaje;
 }
 
-// Prueba eventListener
-// function saludar() {
-//   console.log('hola');
-// }
-
 var miForm = document.querySelector('#formulario');
-
-// selecciona elemento al que quieres vincular el evento
-// miForm.addEventListener("click", saludar); // ejemplo
-/* addEventListener acepta dos parámetros ("evento"(click), funcion) */
 
 miForm.addEventListener('submit', recogeDatos);
 
 
-// También se puede utilizar una función anónima en este ejercicio
-miForm.addEventListener("click", function) {
-  console.log('Prueba');
-}
 
 
